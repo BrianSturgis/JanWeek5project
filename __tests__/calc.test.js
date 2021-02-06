9c8ceb08854edec5390a91481ec6114a7fab35a0
Brian Sturgis <sturujisu@gmail.com>
2021-02-06 14:26:38 -0800

    good pass amnd nor refactor for the Calculator Constructor

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -3,12 +3,13 @@ import Calculator from './../src/js/calcScripts.js';
 describe('Calculator', () => {
   let earthAge;
   beforeEach(() => {
-    earthAge = new Calculator(0,0,0);
+    earthAge = new Calculator(70,0,0);
     
 });
 test('Should create and instance of Calculator', () => {
-  
+  expect(earthAge.age).toEqual(70); 
+  expect(earthAge.pastLife).toEqual(0);
+  expect(earthAge.lifeLeft).toEqual(0);
 });
-
 
 });
