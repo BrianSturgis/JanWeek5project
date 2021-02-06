1c3b72f8cd3dfbaaa9695c89121adcdefd4c3468
Brian Sturgis <sturujisu@gmail.com>
2021-02-06 14:22:18 -0800

    good fail for the Calculator Constructor

--- src/js/calcScripts.js
+++ src/js/calcScripts.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable */
 export default class Calculator {
   constructor (userInput) {
     this.age = userInput;
@@ -7,18 +7,9 @@ export default class Calculator {
     this.alienYears = [];
     this.life = [];
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
+let test = 0;
+let earthAge = new Calculator(0,0,0);
+console.log(earthAge);
+console.log(test);
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-
-// import $ from 'jquery';
+/* eslint-disable */
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Calculator from './js/calcScripts.js';
 
 
-
-// user logic
+// let test = 0;
+// let earthAge = new Calculator(0,0,0);
+// console.log(earthAge);
+// console.log(test);
