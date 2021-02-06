9066b3a6c3c227588b445d9cd1f4f89e753ec752
Brian Sturgis <sturujisu@gmail.com>
2021-02-06 15:49:27 -0800

    good fail/pass for ageCheck fucntion

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -3,14 +3,15 @@ import Calculator from './../src/js/calcScripts.js';
 describe('Calculator', () => {
   let earthAge;
   beforeEach(() => {
-    earthAge = new Calculator(43,0,0);
+    earthAge = new Calculator(43,0,30);
     earthAge.alienAge();
+    earthAge.ageCheck();
     
 });
 test('Should create and instance of Calculator', () => {
   expect(earthAge.age).toEqual(43); 
   expect(earthAge.pastLife).toEqual(0);
-  expect(earthAge.lifeLeft).toEqual(0);
+  expect(earthAge.lifeLeft).toEqual(30);
 });
 test('alienAge should calculate 70 years into Mercury years', () => {
   expect(earthAge.alienYears[0]).toEqual(179);
@@ -24,7 +25,11 @@ test('alienAge should calculate 70 years into Mercury years', () => {
 test('alienAge should calculate 70 years into Mercury years', () => {
   expect(earthAge.alienYears[2]).toEqual(23);
 });
-
+test('ageCheck should calculate the difference of 70 and 73 for determing where total will go to',() => {
+  expect(earthAge.pastLife).toEqual(0);  
+  expect(earthAge.lifeLeft).toEqual(30);
+});
 
 
 });
+
--- src/js/calcScripts.js
+++ src/js/calcScripts.js
@@ -13,10 +13,33 @@ export default class Calculator {
       this.alienYears.push(Math.round(this.age  / array[i]));
     }
   }
+  ageCheck() {
+    let age;
+    let lifeEx = 73;
+    if (this.age == lifeEx || this.age < lifeEx) {
+      age = lifeEx - this.age;
+      this.lifeLeft = age;
+      alert(`The average life expectancy of a Human is 73 giving you ${age} years to live.`);
+    }else if (this.age > lifeEx) {
+      age = this.age - lifeEx;
+      this.pastLife = age;
+      alert(`Science says you have live ${age} years past your species life expectancy.`);
+    }
+  }
+  alienExpectancy(){
+    let array = [0.24, 0.62, 1.88, 11.86];
+    for(let i = 0; i < array.length; i++) {
+      this.life.push(Math.round(this.pastLife / array[i] || this.lifeLeft / array[i]));
+    }
+    
+  }
 }
 
-let test = 0;
-let earthAge = new Calculator(43,0,0);
-console.log(earthAge);
-console.log(test);
+
+let earthAge = new Calculator(43);
+// console.log(earthAge);
+
 earthAge.alienAge();
+earthAge.ageCheck();
+
+console.log(earthAge);
