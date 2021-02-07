370ab3cb2360f6a65d3367a2beace72269083082
Brian Sturgis <sturujisu@gmail.com>
2021-02-06 16:20:13 -0800

    good fail for agecheck ([0])

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -3,7 +3,7 @@ import Calculator from './../src/js/calcScripts.js';
 describe('Calculator', () => {
   let earthAge;
   beforeEach(() => {
-    earthAge = new Calculator(43,0,30);
+    earthAge = new Calculator(43);
     earthAge.alienAge();
     earthAge.ageCheck();
     earthAge.alienExpectancy();
@@ -30,17 +30,37 @@ test('ageCheck should calculate the difference of 70 and 73 for determing where 
   expect(earthAge.pastLife).toEqual(0);  
   expect(earthAge.lifeLeft).toEqual(30);
 });
-test('should calculate 33 years into Mercury years',() => {
+test('should calculate 43 years into Mercury years',() => {
   expect(earthAge.life[0]).toEqual(125);
 });
-test('should calculate 33 years into Mercury years',() => {
+test('should calculate 43 years into Mercury years',() => {
   expect(earthAge.life[1]).toEqual(48);
 });
-test('should calculate 33 years into Mercury years',() => {
+test('should calculate 43 years into Mercury years',() => {
   expect(earthAge.life[2]).toEqual(16);
 });
-test('should calculate 33 years into Mercury years',() => {
+test('should calculate 43 years into Mercury years',() => {
   expect(earthAge.life[3]).toEqual(3);
 });
+test('Should create and instance of Calculator where user is 0 years over the avg lifespan',() => {
+  let overEarthAge = new Calculator (43);
+  overEarthAge.alienAge();
+  overEarthAge.ageCheck();
+  overEarthAge.alienExpectancy();
+  expect(overEarthAge.pastLife).toEqual(0);  
+  expect(overEarthAge.lifeLeft).toEqual(30);
+});
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
